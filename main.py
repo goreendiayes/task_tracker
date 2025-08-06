df5e98aefa7a8b513528955174ea3467d9bde39c
Saer Goree-Ndiaye <gundumgaming13@gmail.com>
2025-08-06 14:45:24 -0700

    'update' changes

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ task_list = []
 
 #'Add' task function
 # Add a new task
-def add_task():
+def add_task(task):
     new_task = input("Enter a task: ")
     task_list.append(new_task)
-    print(f"task: '{new_task}' was added.")
-    
-    # try:
-    #     with open(file_path, "w") as file:
-    #         json.dump(new_task, file, indent=4)
-    #         print(f"json file was created")
-    # except Exception:
-    #     print("Do nothing")
+    # task_id = new_task.ge
+    # print(new_task, task_id)
+    # task_list.append(new_task)
+    # print(f"task: '{new_task}' was added (ID: {new_task.index}).")
 
-new_task = add_task()
+    # with open(file_path, "w", newline="") as file:
+    #     json.dump(new_task, file, indent=4)
+    #     print(f"task: '{new_task}' was added (ID: ).")
+    #     # print(f"json file was created")
+
+
+def update_task():
+    update_task = input("Enter task change: ")
+
+
+add_new_task = add_task("IDK")
+print(task_list)
