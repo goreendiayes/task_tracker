14446e5e18aa1af010336db37b9782f07aef9bc4
Saer Goree-Ndiaye <gundumgaming13@gmail.com>
2025-08-07 00:08:27 -0700

    update write function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ task_list = []
 
 #'Add' task function
 # Add a new task
-def add_task(task):
+def add_task():
     new_task = input("Enter a task: ")
     task_list.append(new_task)
     # task_id = new_task.ge
@@ -18,15 +18,15 @@ def add_task(task):
     # task_list.append(new_task)
     # print(f"task: '{new_task}' was added (ID: {new_task.index}).")
 
-    # with open(file_path, "w", newline="") as file:
-    #     json.dump(new_task, file, indent=4)
-    #     print(f"task: '{new_task}' was added (ID: ).")
-    #     # print(f"json file was created")
+    with open(file_path, "a") as file:
+        json.dump(new_task, file, indent=1000)
+        print(f"task: '{new_task}' was added (ID: ).")
+        # print(f"json file was created")
 
 
 def update_task():
     update_task = input("Enter task change: ")
 
 
-add_new_task = add_task("IDK")
+add_task()
 print(task_list)
