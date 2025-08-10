167c60ba5cae8d482fcf4cab41167905c78e7921
Saer Goree-Ndiaye <gundumgaming13@gmail.com>
2025-08-10 01:50:18 -0700

    refactor add_task function

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 #Some comments
 
-# Import modules
+#Import modules
 import json
+import datetime
 
-# Global variables
+#Global variables
 file_path = "taskList.json"
 
-task_list = []
-
-#'Add' task function
-# Add a new task
 def add_task():
-    new_task = input("Enter a task: ")
-    task_list.append(new_task)
-    # task_id = new_task.ge
-    # print(new_task, task_id)
-    # task_list.append(new_task)
-    # print(f"task: '{new_task}' was added (ID: {new_task.index}).")
+    task_id = 0
+    task_list = {}
+    task = input("Enter a task: ")
+    task_id += 1
+    task_created = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    
+    task_list["Task ID"] = str(task_id) #This needs to increment the value
+    task_list["Description"] = task #capitalizeME
+    task_list["Status"] = "New"
+    task_list["createdAt"] = task_created
 
+    
     with open(file_path, "a") as file:
-        json.dump(new_task, file, indent=1000)
-        print(f"task: '{new_task}' was added (ID: ).")
+        json.dump(task_list, file, indent=4)
+        print(f"task: '{task}' was added (ID: {task_id}).")
         # print(f"json file was created")
 
 
-def update_task():
-    update_task = input("Enter task change: ")
+# def update_task():
+#     task_updated = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+#     update_task = input("Enter task change: ")
 
+# def delete_task():
 
-add_task()
-print(task_list)
+new_task = add_task()
+
